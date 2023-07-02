4e324384f41d235179968e486032270a1a96f026
Akshat Vishwakarma <108259385+akshatV21@users.noreply.github.com>
2023-07-02 13:47:56 +0000

    auth service setup complete

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  await app.listen(3000);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const PORT = configService.get('PORT');
+
+  app.use(helmet())
+  app.use(morgan('dev'));
+
+  await app.listen(PORT, () =>
+    console.log(`Auth service is listening to requests on port: ${PORT}`),
+  );
 }
 bootstrap();
